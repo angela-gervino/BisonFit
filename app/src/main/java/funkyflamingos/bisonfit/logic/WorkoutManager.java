04f1439a0bdd0f2d76560473b7559e460eec9f49
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2023-06-07 11:33:39 -0500

    Add WorkoutManager starting point

--- app/src/main/java/funkyflamingos/bisonfit/logic/WorkoutManager.java
+++ app/src/main/java/funkyflamingos/bisonfit/logic/WorkoutManager.java
@@ -1,4 +1,17 @@
 package funkyflamingos.bisonfit.logic;
 
+import java.util.List;
+
+import funkyflamingos.bisonfit.dso.RoutineHeader;
+import funkyflamingos.bisonfit.persistence.IRoutinesPersistence;
+
 public class WorkoutManager {
+
+    private IRoutinesPersistence persistence;
+    public WorkoutManager(IRoutinesPersistence p) {
+        persistence = p;
+    }
+    public List<RoutineHeader> getAllRoutineHeaders() {
+        return  persistence.getAllRoutineHeaders();
+    }
 }
