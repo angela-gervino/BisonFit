1c4429512c8f203d5fabed4ccd738e572c01dd36
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2023-06-07 17:15:14 -0500

    Removes unnecessary imports

--- app/src/main/java/funkyflamingos/bisonfit/ui/MyWorkoutsListAdapter.java
+++ app/src/main/java/funkyflamingos/bisonfit/ui/MyWorkoutsListAdapter.java
@@ -2,17 +2,14 @@ package funkyflamingos.bisonfit.ui;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.Console;
 import java.util.List;
 
 import funkyflamingos.bisonfit.R;
