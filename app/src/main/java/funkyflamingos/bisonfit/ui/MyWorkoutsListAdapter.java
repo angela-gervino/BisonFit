dd32c1aba89b8bc69a6797faf844ea44834bb959
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2023-06-07 11:14:49 -0500

    Create Adapter class for RecyclerView

---
+++ app/src/main/java/funkyflamingos/bisonfit/ui/MyWorkoutsListAdapter.java
@@ -0,0 +1,49 @@
+package funkyflamingos.bisonfit.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import funkyflamingos.bisonfit.R;
+import funkyflamingos.bisonfit.dso.RoutineSummary;
+
+public class MyWorkoutsListAdapter extends RecyclerView.Adapter<MyWorkoutsListAdapter.ViewHolder>{
+
+    private RoutineSummary[] localDataSet;
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView lblWorkout;
+
+        public ViewHolder(View view) {
+            super(view);
+            //TODO: define click listener for lblWorkout
+            lblWorkout = view.findViewById(R.id.lblWorkoutName);
+        }
+
+        public TextView getLabel() {
+            return lblWorkout;
+        }
+
+    }
+
+    public MyWorkoutsListAdapter(RoutineSummary[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.my_workout_list_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        viewHolder.getLabel().setText(localDataSet[position].getRoutineName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+}
