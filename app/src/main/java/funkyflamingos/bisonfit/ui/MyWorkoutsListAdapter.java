06d6eec43cc2f84a002e6d01a2c5464e8fc62552
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2023-06-07 18:07:29 -0500

    MainActivity and RoutineOverviewActivity pass ID

--- app/src/main/java/funkyflamingos/bisonfit/ui/MyWorkoutsListAdapter.java
+++ app/src/main/java/funkyflamingos/bisonfit/ui/MyWorkoutsListAdapter.java
@@ -19,26 +19,6 @@ public class MyWorkoutsListAdapter extends RecyclerView.Adapter<MyWorkoutsListAd
 
     private List<RoutineHeader> localDataSet;
     private Context parentActivity;
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView lblWorkout;
-        private final ConstraintLayout layout;
-        private final Context parentActivity;
-
-        public ViewHolder(View view, Context parentActivity) {
-            super(view);
-            lblWorkout = view.findViewById(R.id.lblWorkoutName);
-            layout = view.findViewById(R.id.workout_item_layout);
-            this.parentActivity = parentActivity;
-        }
-
-        public TextView getLabel() {
-            return lblWorkout;
-        }
-
-        public ConstraintLayout getLayout() {
-            return layout;
-        }
-    }
 
     public MyWorkoutsListAdapter(List<RoutineHeader> dataSet, Context parentActivity) {
         localDataSet = dataSet;
@@ -49,17 +29,18 @@ public class MyWorkoutsListAdapter extends RecyclerView.Adapter<MyWorkoutsListAd
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.my_workout_list_item, viewGroup, false);
-        return new ViewHolder(view, parentActivity);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
         viewHolder.getLabel().setText(localDataSet.get(position).getName());
+        viewHolder.setDataItem(localDataSet.get(position));
 
         viewHolder.getLayout().setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 Intent intent = new Intent(parentActivity, RoutineOverviewActivity.class);
-                intent.putExtra("routineName",viewHolder.getLabel().getText().toString());
+                intent.putExtra("routineID", viewHolder.getDataItem().getId());
                 parentActivity.startActivity(intent);
             }
         });
@@ -68,5 +49,34 @@ public class MyWorkoutsListAdapter extends RecyclerView.Adapter<MyWorkoutsListAd
     @Override
     public int getItemCount() {
         return localDataSet.size();
+    }
+
+
+    //ViewHolder object holds the individual UI item to display and interact with
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private RoutineHeader dataItem;
+        private final TextView lblWorkout;
+        private final ConstraintLayout layout;
+
+        public ViewHolder(View view) {
+            super(view);
+            lblWorkout = view.findViewById(R.id.lblWorkoutName);
+            layout = view.findViewById(R.id.workout_item_layout);
+        }
+
+        public void setDataItem(RoutineHeader dataItem) {
+            this.dataItem = dataItem;
+        }
+
+        public RoutineHeader getDataItem() {
+            return dataItem;
+        }
+        public TextView getLabel() {
+            return lblWorkout;
+        }
+
+        public ConstraintLayout getLayout() {
+            return layout;
+        }
     }
 }
