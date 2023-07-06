800a2f2278388baccf86f9a14748e0e7672423cd
nehoehne <77899522+nehoehne@users.noreply.github.com>
2023-07-05 21:46:16 -0500

    Fixed activity opening issue

--- app/src/main/java/funkyflamingos/bisonfit/ui/GymHoursActivity.java
+++ app/src/main/java/funkyflamingos/bisonfit/ui/GymHoursActivity.java
@@ -1,15 +1,16 @@
 package funkyflamingos.bisonfit.ui;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 import funkyflamingos.bisonfit.R;
 
 public class GymHoursActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gym_hours_page);
+        setContentView(R.layout.activity_gym_hours);
     }
 }
