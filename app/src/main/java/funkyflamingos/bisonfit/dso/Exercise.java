9288671fdcc359de98e5e729a5f2e231299b0274
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2023-07-07 11:45:35 -0500

    Add all DSO objects from a different branch

---
+++ app/src/main/java/funkyflamingos/bisonfit/dso/Exercise.java
@@ -0,0 +1,47 @@
+package funkyflamingos.bisonfit.dso;
+
+import java.util.ArrayList;
+
+public class Exercise {
+    private ExerciseHeader header;
+    private ArrayList <ExerciseSet> sets;
+
+    public Exercise(String name, int id, int setCount) {
+        this(name, id);
+
+        for(int i = 0; i < setCount; i++) {
+            sets.add(new ExerciseSet(0, 0, (i + 1)));
+        }
+
+    }
+
+    public Exercise(String name, int id) {
+        this(new ExerciseHeader(name, id));
+    }
+
+    // go to constructor
+    public Exercise(ExerciseHeader header) {
+        this.header = header;
+        sets = new ArrayList<>();
+    }
+
+    public ExerciseSet getSet(int pos) {
+        return sets.get(pos);
+    }
+
+    public ArrayList<ExerciseSet> getAllSets() {
+        return sets;
+    }
+
+    public ExerciseHeader getHeader() {
+        return header;
+    }
+
+    public String getName() {
+        return header.getName();
+    }
+
+    public int getID() {
+        return header.getId();
+    }
+}
--- app/src/main/java/funkyflamingos/bisonfit/dso/Routine.java
+++ app/src/main/java/funkyflamingos/bisonfit/dso/Routine.java
@@ -1,20 +1,43 @@
 package funkyflamingos.bisonfit.dso;
 
+import java.util.ArrayList;
 import java.util.Date;
 
+import funkyflamingos.bisonfit.R;
+
 public class Routine {
-    RoutineHeader header;
+    private RoutineHeader header;
+    private final ArrayList<Exercise> exercises;
 
-    // will add more fields later
-
+    // main constructor
     public Routine(RoutineHeader header) {
         this.header = header;
+        exercises = new ArrayList<>();
+    }
+
+    public Routine(String name, int id) {
+        this(new RoutineHeader(name, id));
     }
 
     public RoutineHeader getHeader() {
         return header;
     }
 
+    public Exercise getExercise(int pos) {
+        return exercises.get(pos);
+    }
+
+    public ArrayList<Exercise> getAllExercises() {
+        return exercises;
+    }
+
+    public String getName() {
+        return header.getName();
+    }
+
+    public void addExercise(Exercise exercise) {
+        exercises.add(exercise);
+    }
 
     //need a function that returns all exercise headers as an ArrayList
 
