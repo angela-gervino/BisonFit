03d8b7aa6c18e9a8c92c80f8ba08d2fdd6e6cb93
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2023-07-10 16:24:30 -0500

    Changes to WelcomeActivity.java

--- app/src/main/java/funkyflamingos/bisonfit/ui/WelcomeActivity.java
+++ app/src/main/java/funkyflamingos/bisonfit/ui/WelcomeActivity.java
@@ -4,37 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import funkyflamingos.bisonfit.R;
 import funkyflamingos.bisonfit.logic.IUserRegistrationHandler;
 import funkyflamingos.bisonfit.logic.UserRegistrationHandler;
-import funkyflamingos.bisonfit.persistence.utils.DBHelper;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private IUserRegistrationHandler userNameHandler;
     private String name;
     private EditText editText;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        DBHelper.copyDatabaseToDevice(this);
-        userNameHandler = new UserRegistrationHandler();
+        //android database
+        userNameHandler = new UserRegistrationHandler(this);
+        // new databse
+        // userNameHandler = new UserRegistrationHandler();
         editText = findViewById(R.id.nameEditText);
+        button = findViewById(R.id.btnStartWelcomeActivity);
+
         if (userNameHandler.userHasRegistered()) {
             startHomePageActivity();
         }
-        else {
-        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String newText = editText.getText().toString();
+                button.setEnabled(userNameHandler.userNameValid(newText));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     public void btnStartClicked(View v) {
         name = editText.getText().toString();
-        if (!name.equals("")) {
-            userNameHandler.setUserName(name);
+        if (userNameHandler.setUserName(name)) {
             startHomePageActivity();
         }
     }
