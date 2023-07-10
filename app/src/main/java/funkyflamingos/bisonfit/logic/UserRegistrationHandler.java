486b03b3607f3a200d0aa3f5058ca0d6a7384230
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2023-07-10 16:25:20 -0500

    Changes to UserRegistrationHandler.java

--- app/src/main/java/funkyflamingos/bisonfit/logic/UserRegistrationHandler.java
+++ app/src/main/java/funkyflamingos/bisonfit/logic/UserRegistrationHandler.java
@@ -3,19 +3,24 @@ package funkyflamingos.bisonfit.logic;
 import android.content.Context;
 
 import funkyflamingos.bisonfit.application.Services;
-import funkyflamingos.bisonfit.persistence.IGymHoursPersistence;
 import funkyflamingos.bisonfit.persistence.IUserRegistrationPersistence;
+import funkyflamingos.bisonfit.persistence.UserRegistrationPersistence;
+import funkyflamingos.bisonfit.application.Constants;
 
 public class UserRegistrationHandler implements IUserRegistrationHandler {
 
     private IUserRegistrationPersistence persistence;
 
-    public UserRegistrationHandler(IUserRegistrationPersistence persistence) {
-        this.persistence = persistence;
+    public UserRegistrationHandler(Context context) {
+        persistence = new UserRegistrationPersistence(context);
     }
 
     public UserRegistrationHandler(){
         persistence = Services.getUserRegistrationPersistence();
+    }
+
+    public UserRegistrationHandler(IUserRegistrationPersistence persistence) {
+        this.persistence = persistence;
     }
 
 
@@ -25,12 +30,24 @@ public class UserRegistrationHandler implements IUserRegistrationHandler {
     }
 
     @Override
-    public void setUserName(String userName) {
-        persistence.setUserName(userName);
+    public boolean setUserName(String userName) {
+        userName = userName.trim();
+        if(userNameValid(userName)) {
+            persistence.setUserName(userName);
+            return  true;
+        }
+        return false;
     }
 
     @Override
     public String getUserName() {
         return persistence.getUserName();
+    }
+
+    @Override
+    public boolean userNameValid(String userName) {
+        userName = userName.trim();
+        return (userName.length() >= Constants.MIN_USERNAME_LENGTH
+                && userName.length() <= Constants.MAX_USERNAME_LENGTH);
     }
 }
