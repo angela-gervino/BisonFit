6e5f8904f8ee0d4893709b9c52c639f74d086634
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2023-07-21 15:41:53 -0500

    Implementation of PreviousWorkoutsActivity with fake data

--- app/src/main/java/funkyflamingos/bisonfit/ui/PreviousWorkoutsActivity.java
+++ app/src/main/java/funkyflamingos/bisonfit/ui/PreviousWorkoutsActivity.java
@@ -1,10 +1,16 @@
 package funkyflamingos.bisonfit.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 import funkyflamingos.bisonfit.R;
+import funkyflamingos.bisonfit.dso.PerformedWorkoutHeader;
 
 public class PreviousWorkoutsActivity extends AppCompatActivity {
 
@@ -12,5 +18,21 @@ public class PreviousWorkoutsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_previous_workouts);
+
+        // set up recycler view
+        RecyclerView recyclerView = findViewById(R.id.rvPrevWorkouts);
+        ArrayList<PerformedWorkoutHeader> workoutHeadersToShow = createStubHeaderList();
+        PreviousWorkoutsListAdapter adapter = new PreviousWorkoutsListAdapter(workoutHeadersToShow, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private ArrayList<PerformedWorkoutHeader> createStubHeaderList() {
+        ArrayList<PerformedWorkoutHeader> list = new ArrayList<>();
+        list.add(new PerformedWorkoutHeader("Upper Body", 0, LocalDateTime.now()));
+        list.add(new PerformedWorkoutHeader("Lower Body", 1, LocalDateTime.now()));
+        list.add(new PerformedWorkoutHeader("Full Body", 1, LocalDateTime.now()));
+
+        return list;
     }
 }
